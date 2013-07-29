fe452d0a962cdf24f0430c45bcfe05d7790662f8
Mike OShea <mike@limpidfox.com>
2013-07-28 22:05:17 -0700

    view model directive example

---
+++ AngularSPA/Scripts/directives/observe.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+// Example of observing a value on the model
+
+myApp.directive('mytag', function (restEventData) {  // note you can inject services
+    return {
+        restrict: 'E',
+        template: '<img />',
+        replace: true,
+        link: function (scope, element, ettrs, conrtoller) {
+            $attrs.$observe('email', function (newValue, oldValue) {
+                if (newValue !== oldValue) {
+                    attrs.$set('src', "somenew value");
+                }
+            });
+        }
+    }
+});
