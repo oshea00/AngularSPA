6861ce11a14935e8a026b5672d306322f73ec56e
Mike OShea <oshea00@gmail.com>
2013-08-05 14:37:47 -0700

    convert Upvote to inline template

--- AngularSPA/Scripts/directives/Upvote.js
+++ AngularSPA/Scripts/directives/Upvote.js
@@ -1,8 +1,17 @@
-﻿/// <reference path="../../Content/templates/directives/Upvote.html" />
-myApp.directive('upvote', function () {
+﻿myApp.directive('upvote', function () {
     return {
         restrict: 'E',
-        templateUrl: '/Content/templates/directives/Upvote.html',
+        template: '<div class="span0 well" style="width: 15px;">' +
+            '    <div class="votingButton" ng-click="upvote()" style="width: 0px; margin: 0 auto;">' +
+            '        <i class="icon-chevron-up icon-white"></i>' +
+            '    </div>' +
+            '    <div class="badge badge-inverse">' +
+            '        <div>{{count}}</div>' +
+            '    </div>' +
+            '    <div class="votingButton" ng-click="downvote()" style="width: 0px; margin: 0 auto;">' +
+            '        <i class="icon-chevron-down icon-white"></i>' +
+            '    </div>' +
+            '</div>',
         scope: {
             upvote: "&",
             downvote: "&",
